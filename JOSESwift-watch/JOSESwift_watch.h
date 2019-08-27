f8d680ba7b10bef2c4e76a5a6f32b21567f8adef
Jakub Mejtský <jmejtsky@gmail.com>
2019-08-27 13:52:23 +0200

    Fix watch plattform

---
+++ JOSESwift-watch/JOSESwift_watch.h
@@ -0,0 +1,18 @@
+//
+//  JOSESwift_watch.h
+//  JOSESwift-watch
+//
+//  Created by Jakub Mejtský on 27/08/2019.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for JOSESwift_watch.
+FOUNDATION_EXPORT double JOSESwift_watchVersionNumber;
+
+//! Project version string for JOSESwift_watch.
+FOUNDATION_EXPORT const unsigned char JOSESwift_watchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JOSESwift_watch/PublicHeader.h>
+
+
