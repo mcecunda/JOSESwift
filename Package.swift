deb23639a23637404eb9203c4e88057663a5fd57
Mejtský Jakub <jakub.mejtsky@ahead-itec.com>
2020-12-01 15:13:07 +0100

    Add support as SPM dependency

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "JOSESwift",
+    platforms: [
+        .iOS(.v11),
+        .watchOS(.v4)
+    ],
+    products: [
+        .library(
+            name: "JOSESwift",
+            type: .dynamic,
+            targets: ["JOSESwift"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "JOSESwift", dependencies: [], path: "JOSESwift/Sources"),
+    ]
+)
